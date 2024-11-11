57dd2e73c6a24d88a2287699cb2ce64a2c80b2ba
froZin <bolutolu12@gmail.com>
2024-11-11 03:29:18 +0100

    all done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const generateBtn = document.getElementById("btn");
+let count = 0;
+
+generateBtn.addEventListener("click", () => {
+    
+
+
+    fetch("https://api.adviceslip.com/advice?timestamp=<random_number>").then(response => {
+        if(!response.ok){
+            throw new Error(window.alert("Couldn't load advices"));
+        }
+        return response.json();
+    }).then(data => getData(data)).catch(error => window.alert(error));
+
+ });
+
+ function getData(data){
+    const advice = data.slip.advice;
+    const id = data.slip.id
+    displayAdvice(advice, id);    
+}
+
+function displayAdvice(advice){
+    const textBox = document.getElementById("quote");
+    const adviceCount = document.getElementById("adviceCount");
+
+    textBox.innerText = advice;
+    count++;
+    adviceCount.textContent = count;
+}
 
